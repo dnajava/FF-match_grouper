f3bfc06553b8a408f669a17a4e90bca07fa3015c
Ilpo Kantonen <ilkant@users.noreply.github.com>
2023-08-27 09:14:04 +0300

    Add files via upload
    Main program. It calls Matchlists and others.

---
+++ main.py
@@ -0,0 +1,10 @@
+# FF match grouper
+# Ilpo Kantonen 27 August 2023
+
+from Matchlists import Matchlists
+
+if __name__ == '__main__':
+    ml = Matchlists()
+    ml.get_matchlists('/mnt/sda4/Lataukset/')
+
+    ml.mkhtmltable()
