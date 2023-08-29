e49820422fd6fa8b35c9aa519f1a6ac76bb3354f
Ilpo Kantonen <ilkant@users.noreply.github.com>
2023-08-29 18:45:54 +0300

    Add files via upload
    New features. Use colors with CSS file. Different colors to common matches and diagonal line and unused table cells. Headers also can be coloured and the angle cell can be different color. Now there can be kit identification and real names of tested persons.

--- Matchlists.py
+++ Matchlists.py
@@ -1,6 +1,23 @@
 import os
 from datetime import date
 from Matches import Matches
+from KitNames import KitNames
+
+names = [
+    'Pekka Hietala',
+    'Jorma Hietala',
+    'Mauri Peltola',
+    'Johannes Sankari',
+    'Marja Kantonen',
+    'Outi Nieminen',
+    'Paavo Vuorialho',
+    'Matti Matikainen',
+    'Taru Kantonen',
+    'Ritva Kantonen',
+    'Nisa Khan',
+    'Terttu Hietala',
+    'Ilpo Kantonen',
+    'Timo Rehtonen']
 
 class Matchlists:
     lists = []
@@ -65,7 +82,7 @@ class Matchlists:
             i += 1
         print('</table></body></html>')
 
-    def mkhtmltable(self, mode=1):
+    def mkhtmltable(self, mode_p=0):
         print('Making html-table...')
         mllength = len(self.lists)
         f = open("matchtable.html", "w")
@@ -75,33 +92,49 @@ class Matchlists:
 
         # Body. First table header to html file
         f.write('<body>\n<table>\n')
-        s = '<tr><td colspan = "' + str(mllength+1) + '"><h1>Common Matches Table</h1></td></tr>\n'
+        s = '<tr><td colspan = "' + str(mllength+1) + '"class="TableHeader"><h1>Common Matches Table</h1></td></tr>\n'
         f.write(s)
-        f.write('<tr><th>&nbsp;</th>')
-        for ih in range(0, mllength):                         # Header row; kits numbers or identification
-            s = '<th>' + self.lists[ih].kit + '</th>'
-            f.write(s)
+        f.write('<tr><th id="Base">&nbsp;</th>')
+        for ih in range(0, mllength):                           # Header row; kits numbers or identification
+            f.write('<th>')
+            f.write(str(ih+1))
+            f.write('</th>')
         f.write('</tr>\n')
 
         i = 0                                                   # i is list index number and rows after headers start 0
         for row in self.lists:                                  # row index line
-            s = '<tr><th>' + self.lists[i].kit + '</th>'
-            f.write(s)
+            f.write('<tr><th>')
+            match mode_p:
+                case 0:
+                    f.write(str(i + 1))
+                case 1:
+                    s = list[i].kit + str(i+1)
+                    f.write(s)
+                case 2:
+                    s = self.lists[i].kit + ' ' + str(i + 1)
+                    f.write(s)
+                case 3:
+                    s = names[i] + ' ' + self.lists[i].kit + ' ' + str(i+1)
+                    f.write(s)
+                case _:
+                    f.write('&nbsp')
+            f.write('</th>')
+            j = 0
             for i2 in range(0, i):                              # print common matches to diagonal - 1
-                s = '<td>' + str(self.samoja(0, i-1)) + '</td>'
+                j += 1
+                s = '<td class="Before">' + str(self.samoja(i,j)) + '</td>'
                 f.write(s)
-            len3 = len(self.lists[i].matchlist)
-            s = '<td>' + str(len3) + '</td>'
+            len3 = len(self.lists[i].matchlist)                 # Diagonal is match list lenght (all are matches)
+            s = '<td class="Diagonal">' + str(len3) + '</td>'
             f.write(s)
             for i3 in range(i+1, mllength):
-                f.write('<td>&nbsp;</td>')
+                f.write('<td class="Rest">&nbsp;</td>')
             f.write('</tr>')
             i += 1
             if i % 5 == 0:
                 print(i, end=" ")
             else:
                 print('.', end=" ")
-
 
         f.write('\n</table>\n</body>\n</html>')
         f.close()
