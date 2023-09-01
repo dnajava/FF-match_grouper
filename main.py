84fe8a74affc0c355f5f1d0301b7ba1ddaeab0f5
Ilpo Kantonen <ilkant@users.noreply.github.com>
2023-09-01 06:42:28 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -4,13 +4,14 @@
 from Matchlists import Matchlists
 
 if __name__ == '__main__':
+    DL = '/home/ilpo/Lataukset/'
     ml = Matchlists()
-    ml.get_matchlists('/path/to/data/')
+    ml.get_FTDNA_matchlists(DL)
+    ml.get_MyHeritage_matchlists(DL)
+    ml.readkitsnames()
 
     # Parameter:
     # 0, only row number to left column number
     # 1, kit id and number of row to left
     # 2, name of tested person, kit id and row number to left
     ml.mkhtmltable(2)
-
-    # ml.mllengths()
