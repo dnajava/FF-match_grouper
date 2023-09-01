6348b30fb99f6834b85a220a0379102f447d43d4
Ilpo Kantonen <ilkant@users.noreply.github.com>
2023-09-02 00:48:03 +0300

    Add files via upload
    Comments added.

--- Matches.py
+++ Matches.py
@@ -1,13 +1,16 @@
 from csv import reader
 
+'''
+Matchlist of one kit.
+'''
 class Matches:
-    kit = None
-    date = None
+    type = None                         # Type of mach list
+    kit = None                          # Kit id
+    date = None                         # Date of match list
+    # name = None
     matchlist = []
 
-    def __Matches__(self, kit_p: str, day_p: date):
-        self.kit = kit_p
-        self.date = day_p
+    def __Matches__(self):
         return
 
     def setkit(self, kit_p):
@@ -16,7 +19,12 @@ class Matches:
     def setday(self, day_p: date):
         self.date = day_p
 
-    def read_matchlist(self, fname_p='') -> bool:
+    '''
+    Read FTDNA matchlist.
+    '''
+    @param fname_p File name of match file.
+    @return Did the reading succeed?
+    def read_ftdna_matchlist(self, fname_p='') -> bool:
         if fname_p == '':
             return False
         tempkits = []
@@ -37,6 +45,39 @@ class Matches:
                 read_obj.close()
         self.matchlist = tempkits
         return True
+
+    '''
+    Read MyHeritage matchlist.
+    '''
+    @param fname_p File name of match file.
+    @return Did the reading succeed?
+    def read_MyHeritageMatchlist(self, fname_p='') -> bool:
+        if fname_p == '':
+            return False
+        tempkits = []
+        try:
+            with open(fname_p, 'r') as read_obj:
+                csv_reader = reader(read_obj)
+                ekarivi = True  # Skip first header row of match list
+                for m in csv_reader:
+                    if ekarivi == False:
+                        tempkits.append(m[1])
+                    else:
+                        ekarivi = False
+        except (IOError, OSError) as err:
+            print(err)
+            return False
+        finally:
+            if read_obj is not None:
+                read_obj.close()
+        self.matchlist = tempkits
+        return True
+
+    '''
+    For test purposes. Print kit id and date of file. Extended test print also names of matches.
+    '''
+    @param fname_p File name of match file.
+    @return Did the reading succeed?
 
     def show(self, mode=1):
         print('Kit', self.kit, self.date)
